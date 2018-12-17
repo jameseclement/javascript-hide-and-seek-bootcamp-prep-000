7b1bc9c5a8c3a47b94c6f5ba0713909af93fbf9e
jameseclement <jameseclement@gmail.com>
2018-12-17 16:56:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ return document.querySelector('div#nested div.target')
 function increaseRankBy(n) {
  const lis = document.querySelectorAll('ul.ranked-list li')
  for (let i = 0; i < lis.length; i++) {
-   lis[i].innerHTML = (i + parseInt(n).toString())
+   lis[i].innerHTML = (i + (parseInt(n).toString()))
  }
 }
